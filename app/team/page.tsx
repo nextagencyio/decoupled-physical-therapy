acbac6b2de2af1b054cfed26a21564c0ad960bb6
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import TherapistCard from '../components/TherapistCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Therapists | Physical Therapy',
